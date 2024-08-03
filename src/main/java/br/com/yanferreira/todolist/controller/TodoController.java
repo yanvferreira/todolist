0ae4bfa4ab4d332a3c5544e70821a6d877b6be34
Yan Ferreira <yanfvc@gmail.com>
2024-08-03 16:36:26 -0300

    FEAT: Criando métodos e construtor da classe TodoController

--- src/main/java/br/com/yanferreira/todolist/controller/TodoController.java
+++ src/main/java/br/com/yanferreira/todolist/controller/TodoController.java
@@ -1,10 +1,36 @@
 package br.com.yanferreira.todolist.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.yanferreira.todolist.entity.Todo;
+import br.com.yanferreira.todolist.service.TodoService;
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    List<Todo> create(Todo todo){
+        return todoService.create(todo);
+    }
+
+    List<Todo> list(){
+
+    }
+
+    List<Todo> update(){
+
+    }
+
+    List<Todo> delete(){
+
+    }
 
 }
