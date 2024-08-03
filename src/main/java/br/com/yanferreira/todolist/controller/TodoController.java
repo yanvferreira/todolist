b8e6edbfc53e48217e1e84eeeddaef4040f79aea
Yan Ferreira <yanfvc@gmail.com>
2024-08-03 16:47:37 -0300

    FEAT: Implementando os verbos HTTP no controller

--- src/main/java/br/com/yanferreira/todolist/controller/TodoController.java
+++ src/main/java/br/com/yanferreira/todolist/controller/TodoController.java
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.yanferreira.todolist.entity.Todo;
 import br.com.yanferreira.todolist.service.TodoService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
@@ -17,19 +25,23 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    List<Todo> create(Todo todo){
+    @PostMapping 
+    List<Todo> create(@RequestBody Todo todo){
         return todoService.create(todo);
     }
 
+    @GetMapping
     List<Todo> list(){
         return todoService.list();
     }
 
-    List<Todo> update(Todo todo){
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
     }
 
-    List<Todo> delete(Long id){
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
         return todoService.delete(id);
     }
 
