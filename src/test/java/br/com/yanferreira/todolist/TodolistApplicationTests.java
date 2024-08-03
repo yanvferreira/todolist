d031d41946cdd285d4d353a4a296607772b24c4c
Yan Ferreira <yanfvc@gmail.com>
2024-08-03 19:19:02 -0300

    UPDATE: Inserindo webEnviroment para o teste subir numa porta aleatória

--- src/test/java/br/com/yanferreira/todolist/TodolistApplicationTests.java
+++ src/test/java/br/com/yanferreira/todolist/TodolistApplicationTests.java
@@ -3,11 +3,12 @@ package br.com.yanferreira.todolist;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import br.com.yanferreira.todolist.entity.Todo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodolistApplicationTests {
 
 	@Autowired
