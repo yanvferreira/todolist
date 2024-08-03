3f1a75c36b5568b042ad0e9c7100f46219926fc9
Yan Ferreira <yanfvc@gmail.com>
2024-08-03 00:41:32 -0300

    FEAT: Inserindo o retorno do método list() em todos os outros métodos. Assim que finalizar a ação será exibido todas as tarefas.

--- src/main/java/br/com/yanferreira/todolist/service/TodoService.java
+++ src/main/java/br/com/yanferreira/todolist/service/TodoService.java
@@ -18,16 +18,18 @@ public class TodoService {
 
     public List<Todo> create(Todo todo){
         todoRepository.save(todo);
+        return list();
     }
     public List<Todo> list(){
         Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+
         return todoRepository.findAll(sort);
     }
     public List<Todo> update(){
-
+        return list();
     }
     public List<Todo> delete(){
-
+        return list();
     }
 
 }
