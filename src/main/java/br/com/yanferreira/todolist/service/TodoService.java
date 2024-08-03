44e8597a40096b75149a92109192898dcf20a168
Yan Ferreira <yanfvc@gmail.com>
2024-08-03 00:33:17 -0300

    FEAT: Injeção de dependências utilizando contrutor da classe TodoService

--- src/main/java/br/com/yanferreira/todolist/service/TodoService.java
+++ src/main/java/br/com/yanferreira/todolist/service/TodoService.java
@@ -1,8 +1,31 @@
 package br.com.yanferreira.todolist.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import br.com.yanferreira.todolist.entity.Todo;
+import br.com.yanferreira.todolist.repository.TodoRepository;
 
 @Service
 public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(){
+
+    }
+    public List<Todo> list(){
+
+    }
+    public List<Todo> update(){
+
+    }
+    public List<Todo> delete(){
+
+    }
 
 }
